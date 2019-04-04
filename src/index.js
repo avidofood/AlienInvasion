6b4aba46c1bb2ad46177d6ac3806f99b269ac6d1
Philipp <p@mochine.de>
2019-04-04 16:23:47 +0200

    Sprite

--- src/index.js
+++ src/index.js
@@ -259,35 +259,34 @@ const GameBoard = function () {
     };
 };
 
-/* eslint-disable */
 
-var Sprite = function() { };
+const Sprite = function () { };
 
-Sprite.prototype.setup = function(sprite,props) {
-  this.sprite = sprite;
-  this.merge(props);
-  this.frame = this.frame || 0;
-  this.w =  SpriteSheet.map[sprite].w;
-  this.h =  SpriteSheet.map[sprite].h;
+Sprite.prototype.setup = function (sprite, props) {
+    this.sprite = sprite;
+    this.merge(props);
+    this.frame = this.frame || 0;
+    this.w = SpriteSheet.map[sprite].w;
+    this.h = SpriteSheet.map[sprite].h;
 };
 
-Sprite.prototype.merge = function(props) {
-  if(props) {
-    for (var prop in props) {
-      this[prop] = props[prop];
+Sprite.prototype.merge = function (props) {
+    if (props) {
+        Object.keys(props).forEach((prop) => {
+            this[prop] = props[prop];
+        });
     }
-  }
 };
 
-Sprite.prototype.draw = function(ctx) {
-  SpriteSheet.draw(ctx,this.sprite,this.x,this.y,this.frame);
+Sprite.prototype.draw = function (ctx) {
+    SpriteSheet.draw(ctx, this.sprite, this.x, this.y, this.frame);
 };
 
-Sprite.prototype.hit = function(damage) {
-  this.board.remove(this);
+Sprite.prototype.hit = function () {
+    this.board.remove(this);
 };
 
-
+/* eslint-disable */
 var Level = function(levelData,callback) {
   this.levelData = [];
   for(var i =0; i<levelData.length; i++) {
