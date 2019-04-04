0cfcf18fe2e02e869ea1214051c01bdd899bf40f
Philipp <p@mochine.de>
2019-04-04 17:05:37 +0200

    Level: Updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-alert */
 /* eslint-disable func-names */
@@ -286,59 +287,61 @@ Sprite.prototype.hit = function () {
     this.board.remove(this);
 };
 
-/* eslint-disable */
-var Level = function(levelData,callback) {
-  this.levelData = [];
-  for(var i =0; i<levelData.length; i++) {
-    this.levelData.push(Object.create(levelData[i]));
-  }
-  this.t = 0;
-  this.callback = callback;
-};
 
-Level.prototype.step = function(dt) {
-  var idx = 0, remove = [], curShip = null;
-
-  // Update the current time offset
-  this.t += dt * 1000;
-
-  //   Start, End,  Gap, Type,   Override
-  // [ 0,     4000, 500, 'step', { x: 100 } ]
-  while((curShip = this.levelData[idx]) && 
-        (curShip[0] < this.t + 2000)) {
-    // Check if we've passed the end time 
-    if(this.t > curShip[1]) {
-      remove.push(curShip);
-    } else if(curShip[0] < this.t) {
-      // Get the enemy definition blueprint
-      var enemy = enemies[curShip[3]],
-          override = curShip[4];
-
-      // Add a new enemy with the blueprint and override
-      this.board.add(new Enemy(enemy,override));
-
-      // Increment the start time by the gap
-      curShip[0] += curShip[2];
+const Level = function (levelData, callback) {
+    this.levelData = [];
+    for (let i = 0; i < levelData.length; i += 1) {
+        this.levelData.push(Object.create(levelData[i]));
     }
-    idx++;
-  }
-
-  // Remove any objects from the levelData that have passed
-  for(var i=0,len=remove.length;i<len;i++) {
-    var remIdx = this.levelData.indexOf(remove[i]);
-    if(remIdx != -1) this.levelData.splice(remIdx,1);
-  }
-
-  // If there are no more enemies on the board or in 
-  // levelData, this level is done
-  if(this.levelData.length === 0 && this.board.cnt[OBJECT_ENEMY] === 0) {
-    if(this.callback) this.callback();
-  }
-
+    this.t = 0;
+    this.callback = callback;
 };
 
-Level.prototype.draw = function(ctx) { };
+Level.prototype.step = function (dt) {
+    let idx = 0;
+    const remove = [];
+    let curShip = null;
 
+    // Update the current time offset
+    this.t += dt * 1000;
+
+    //   Start, End,  Gap, Type,   Override
+    // [ 0,     4000, 500, 'step', { x: 100 } ]
+    // eslint-disable-next-line no-cond-assign
+    while ((curShip = this.levelData[idx]) && (curShip[0] < this.t + 2000)) {
+        // Check if we've passed the end time
+        if (this.t > curShip[1]) {
+            remove.push(curShip);
+        } else if (curShip[0] < this.t) {
+            // Get the enemy definition blueprint
+            const enemy = enemies[curShip[3]];
+            const override = curShip[4];
+
+            // Add a new enemy with the blueprint and override
+            this.board.add(new Enemy(enemy, override));
+
+            // Increment the start time by the gap
+            curShip[0] += curShip[2];
+        }
+        idx += 1;
+    }
+
+    // Remove any objects from the levelData that have passed
+    for (let i = 0, len = remove.length; i < len; i += 1) {
+        const remIdx = this.levelData.indexOf(remove[i]);
+        if (remIdx !== -1) this.levelData.splice(remIdx, 1);
+    }
+
+    // If there are no more enemies on the board or in
+    // levelData, this level is done
+    if (this.levelData.length === 0 && this.board.cnt[OBJECT_ENEMY] === 0) {
+        if (this.callback) this.callback();
+    }
+};
+
+Level.prototype.draw = function (ctx) { };
+
+/* eslint-disable */
 
 var TouchControls = function() {
 
